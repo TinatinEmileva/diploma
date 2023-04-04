2bf8c6c7b7d6dd63b241436338913e0b1b151243
Emileva Tina <emilevatinatin@gmail.com>
2023-04-04 15:17:23 +0600

    added new paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ function App() {
     <div className="App">
       <h1>Hello world</h1>
     <p>This is my diploma</p>
+    <p>hello world</p>
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Reprehenderit eaque quae repellat delectus veniam 
+         facilis iste esse eos sed cupiditate, quo quisquam in quasi corrupti cum
+          aspernatur atque numquam quas!</p>
+    </div>
     </div>
   )
 }
