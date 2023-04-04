15eddcc13d2fa786f874896f9747e1ab9a1de221
Emileva Tina <emilevatinatin@gmail.com>
2023-04-04 15:18:30 +0600

    added new comment

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ function App() {
       <h1>Hello world</h1>
     <p>This is my diploma</p>
     <p>hello world</p>
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-         Reprehenderit eaque quae repellat delectus veniam 
-         facilis iste esse eos sed cupiditate, quo quisquam in quasi corrupti cum
-          aspernatur atque numquam quas!</p>
-    </div>
+    
     </div>
   )
 }
