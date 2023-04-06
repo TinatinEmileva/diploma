371facd0faec61047a5541defffedaf9b385ad7b
Emileva Tina <emilevatinatin@gmail.com>
2023-04-06 15:55:11 +0600

    Added basic Layout  styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ export default function About(){
   return(
     <div className="About">
       <h1> Furniture Shop</h1>
-      <p>This is my awesome Furniture shop.</p>
+      <p> Furniture  for kitchen.</p>
       <p>We only use naturel ingredients to make our furniture. </p>
     </div>
   );
--- src/pages/Cantacts.jsx
+++ src/pages/Cantacts.jsx
@@ -1,8 +1,8 @@
 export default function Cantacts(){
   return(
     <div className="Cantacts">
-      <h1>This my Furniture Shop</h1>
-      <p>This is my awesome Furniture shop.</p>
+      <h1>Furniture Shop </h1>
+      <p>Adress:Toktogul st.</p>
       <p>Our cantacts:00000 </p>
     </div>
   );
