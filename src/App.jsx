217087a2f40dcb31e8ded61d7affcf8271466a63
Emileva Tina <emilevatinatin@gmail.com>
2023-04-06 14:43:00 +0600

    change all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 
+import { NavLink, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout/Layout'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
-    <p>This is my diploma</p>
-    <p>hello world</p>
-    
+     <Layout>
+     <Routes>
+        <Route path="/" element={<h2>Home </h2>} />
+        <Route path="/about" element={<h2> About  </h2>}  />
+      </Routes>
+     </Layout>
+
     </div>
   )
 }
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+  import Nav from "../../Nav/Nav";
+import "./Layout.css";
+
+  export default function Layout (props){
+    return(
+      <div className="Layout">
+        <header>
+          <Nav/>
+        </header>
+
+        <main>
+        {props.children}
+        </main>
+
+      </div>
+    );
+  }
