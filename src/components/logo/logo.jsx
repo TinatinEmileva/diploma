c01a9905274af4108af130128cd2f1ba58656175
Emileva Tina <emilevatinatin@gmail.com>
2023-04-06 16:28:01 +0600

    Styling Logo

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -3,7 +3,7 @@ import "./Logo.css";
 export default  function Logo(){
   return(
     <div className="Logo">
-      <img src={} alt="Website logo" />   
+      <img src={logo} alt="Website logo" />   
        </div>
-  )
+  );
 }
