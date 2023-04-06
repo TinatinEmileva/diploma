c8b89959ac747dbfc92c5fa9149f5d1af4e899af
Emileva Tina <emilevatinatin@gmail.com>
2023-04-06 14:56:39 +0600

    Added new comment

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,10 +7,15 @@ import "./Layout.css";
         <header>
           <Nav/>
         </header>
-
+        <aside>
+          ASIDE
+        </aside>
         <main>
         {props.children}
         </main>
+        <footer>
+          FOOTER
+        </footer>
 
       </div>
     );
