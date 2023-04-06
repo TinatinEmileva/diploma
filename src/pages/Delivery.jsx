f9db08630d874c2d3f759e0149d503141b7c8307
Emileva Tina <emilevatinatin@gmail.com>
2023-04-06 16:05:43 +0600

    Added new commit

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -1,7 +1,7 @@
 export default function Delivery(){
   return(
     <div className="Delivery">
-      <h1>Welcome to my Furniture Shop</h1>
+      <h1>Welcome to our Shop</h1>
       <p>This is my awesome Furniture shop.</p>
       <p>We only use naturel ingredients to make our furniture. </p>
     </div>
