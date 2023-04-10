befe45ec2c993b69dadcdeae6b47e335000e81f2
Emileva Tina <emilevatinatin@gmail.com>
2023-04-10 23:52:51 +0600

    change p

--- src/pages/Storage.jsx
+++ src/pages/Storage.jsx
@@ -3,7 +3,15 @@ export default function Storage(){
     <div className="Storage">
       <h1>Welcome to my Furniture Shop</h1>
       <p>This is my awesome Furniture shop.</p>
-      <p>We only use naturel ingredients to make our furniture. </p>
+      <p>By type:
+Coffee tables
+Dining tables
+Bedside tables
+Console tables
+Side tables
+Dressing tables
+Desks
+Nesting tables </p>
     </div>
   );
 }
