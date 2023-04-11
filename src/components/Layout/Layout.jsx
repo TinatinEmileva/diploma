1e54fe65947ae772dfd15b76433aa4529533c232
Emileva Tina <emilevatinatin@gmail.com>
2023-04-11 16:15:47 +0600

    fixed errors

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,21 @@
+import Category from "../../pages/Category";
+import CategoryList from "../CategoryList/CategoryList";
 import Logo from "../logo/logo";
 import Nav from "../Nav/Nav";
 import "./Layout.css";
 
+
 export default function Layout(props) {
-  return (  
+  return (
     <div className="Layout">
       <header>
-        <Logo/>
+        <Logo />
         <Nav />
       </header>
-      <aside>Category</aside>
+      <aside>
+        <Category />
+        <CategoryList />
+      </aside>
       <main>{props.children}</main>
       <footer>FOOTER</footer>
     </div>
