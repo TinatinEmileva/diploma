fb3652ae44ad378ec04b92133e969bcae63d028d
Emileva Tina <emilevatinatin@gmail.com>
2023-05-12 10:38:34 +0600

    delete category

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from "react";
 import "./CategoryList.css";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
+import AddCategory from "../AddCategory/AddCategory";
+import DeleteCategory from "../DeleteCategory/DeleteCategory";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
@@ -11,6 +13,7 @@ export default function CategoryList() {
       <NavLink to={'/category/' + category.path}>
         {category.name}
       </NavLink>
+      <DeleteCategory category={category} />
     </li>
   ));
 
@@ -19,6 +22,7 @@ export default function CategoryList() {
       <ul>
         {output}
       </ul>
+      <AddCategory/>
     </div>
   );
 }
