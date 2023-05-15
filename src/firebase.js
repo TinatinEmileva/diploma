cd1cffffae7847d6ac7b62e0ca59abfc4f2d6036
Emileva Tina <emilevatinatin@gmail.com>
2023-05-15 14:01:38 +0600

    change lines

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore, onSnapshot } from "firebase/firestore";
-import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:"AIzaSyDvuYRwBWfxNXjvgvgyHS3rn12Nq4WavF0",
+  apiKey: "AIzaSyDvuYRwBWfxNXjvgvgyHS3rn12Nq4WavF0",
   authDomain: "diploma-57e14.firebaseapp.com",
   projectId: "diploma-57e14",
   storageBucket: "diploma-57e14.appspot.com",
@@ -17,11 +25,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const auth = getAuth(app);
+export const storage = getStorage(app);
 
 // получить список категорий (коллекция документов).
-export const categoryCollection = collection(db, 'categories');
-export const productCollection = collection(db, 'products');
-export const orderCollection = collection(db, 'orders');
+export const categoryCollection = collection(db, "categories");
+export const productCollection = collection(db, "product");
+export const orderCollection = collection(db, "orders");
 
 const provider = new GoogleAuthProvider();
 export const logIn = () => signInWithPopup(auth, provider);
@@ -55,3 +64,12 @@ export const onOrdersLoad = (callback) =>
       }))
     )
   );
+
+// отправка фотографии и получение ее url
+export const uploadProductPhoto = async (file) => {
+  const storageRef = ref(storage, `products/${file.name}`);
+  await uploadBytes(storageRef, file);
+
+  const url = await getDownloadURL(storageRef);
+  return url;
+};
