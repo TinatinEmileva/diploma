f4ab67c5279d90e510425c46ff40ad6e73ce1669
Emileva Tina <emilevatinatin@gmail.com>
2023-05-25 16:07:01 +0600

    add new

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -3,11 +3,14 @@ export default function Delivery(){
   return(
     <div className="Delivery">
       <div className="container">
-      <img src="https://i.pinimg.com/564x/dd/b0/13/ddb01355fb994d3e630f3f946b098d6f.jpg" alt="" />
-      <p>DELIVERY BY THE TRANSPORT COMPANY IS CARRIED OUT ALL OVER THE TERRITORY OF KYRGYZSTAN.<br></br>
-        Delivery of bulky goods to the regions is carried out only by transport companies/<br>
-        </br>100% payment is required to send your order for shipment.<br>
-        </br>Cashless payment through a bank, or payment on-line with a plastic card..</p>
+      <img src="https://i.pinimg.com/564x/bd/08/1e/bd081e0d7d256ef53cc24c040859faaf.jpg" alt="" />
+     <div className="text-del">
+     <h1>Delivery</h1>
+      <p>- DELIVERY BY THE TRANSPORT COMPANY IS CARRIED OUT ALL OVER THE TERRITORY OF KYRGYZSTAN.</p>
+       <p> - Delivery of bulky goods to the regions is carried out only by transport companies</p>
+        <p>- 100% payment is required to send your order for shipment.</p>
+        <p>- Cashless payment through a bank, or payment on-line with a plastic card..</p>
+        </div>
     </div>
     </div>
   );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,11 +5,18 @@ export default function NotFound() {
       <div className="container">
       <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor:%3A053bd947d462cc1a45aeba4070defff75501905071c0eaf68436ac9976ec698c&amp;width=600&amp;height=450&amp;">
 </script>
-        <img src="https://i.pinimg.com/564x/00/87/07/008707505f84474204a45396e610025c.jpg" alt="" />
+        <img src="https://i.pinimg.com/564x/2f/bd/9b/2fbd9b60f6d2202e9f3c5b01a769c153.jpg" alt="" />
+       
         <div className="text-notfound">
-      <h1>Contact</h1>
-      <p>Our contacts: +996 709 312 709
-      </p>
+      <h3>Do you have any question?</h3>
+         <a className="phone" href="/contacts" title="Call the stor" >+996 709 312 709
+         <br />
+         +996 704 575 733</a>
+         <h2>Adress</h2>
+         <p>Kyrgyz Repablik</p>
+         <p>Karakol city</p>
+         <p>Esenina №1</p>
+            
       
       </div>
       </div>
