081e018fae475ba9d6b0f126994b0b08fd872247
Emileva Tina <emilevatinatin@gmail.com>
2023-05-26 14:40:07 +0600

    added new color

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,10 @@ export default function About(){
     <div className="About">
       <div className="container">
        <div className="text-about">
-     <video src="https://www.shatura.com/upload/iblock/b4d/efbrjzlarzb5gq8ab99l3uyw2926yhqa.mp4"></video>
+    <video src="https://www.shatura.com/upload/iblock/b4d/efbrjzlarzb5gq8ab99l3uyw2926yhqa.mp4"></video>
+     <div className="text-about">
+     <p>With modern furniture for your home, comfort meets style to create a beautiful and livable space. Mix and match details such as wood furniture finishes in varying intensity and upholstery in contrasting or complementary colors. This will create a curated feel as you move from room to room. If you frequently entertain in your dining room, look for dining tables with elevated details and room for plenty of seating. For casual family dinners, choose multifunctional furniture that shows off casual style, such as a kitchen cart with space for everyone to gather and help with meal prep. For the fastest delivery, shop already in-stock furniture online or at a furniture store near you</p>
+       </div>
       </div>
       </div>
     </div>
